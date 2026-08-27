cffd976185a580e1aefa4ae047be51b47409bb30
Dominic Neo <dom.neo.ws@gmail.com>
2026-08-27 10:09:49 +0800

    feat: added vercel speed insights

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 
 	import favicon from '$lib/assets/favicon.svg';
 	import fontsCss from '$lib/assets/ds/fonts.css?url';
